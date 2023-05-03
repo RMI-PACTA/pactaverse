c2001b83b6f9ecf2f068c2acaf96690da7718c24
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-05-03 15:45:06 +0200

    add pactaverse function

---
+++ R/pactaverse.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Suppress R CMD check note
+# Namespace in Imports field not imported from: PKG
+#   All declared Imports should be used.
+ignore_unused_imports <- function() {
+  pacta.portfolio.import::determine_headers
+  pacta.data.validation::fake_abcd_flags_bonds
+  pacta.portfolio.utils::abort_if_file_doesnt_exist
+  pacta.executive.summary::alignment_scores_values
+}
+
+release_bullets <- function() {
+  c(
+    '`usethis::use_latest_dependencies(TRUE, "CRAN")`',
+    '`pactaverse_dependency_dissuade()`'
+  )
+}
